66130e6f0cb838036825f0aad698738f427b7fb9
Quaid DeLacluyse <quaidd@lesman.com>
2019-10-14 22:18:59 -0400

    Added green-score page

---
+++ Scripts/site.js
@@ -0,0 +1,17 @@
+
+// Initialize the SideNav and Tabs
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('.modal').modal();
+});
+
+/**
+    Function to switch to a different tab using the Materialize function
+    @param tabToSwitchTo - String refernece to the ID of the content's div
+*/
+function switchTab(tabToSwitchTo) {
+    var elem = document.querySelector('.tabs');
+    var instance = M.Tabs.getInstance(elem);
+    instance.select(tabToSwitchTo);
+}
