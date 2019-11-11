cc3c838c594f53d104248cf7e93fd701c4b19771
Quaid DeLacluyse <quaidd@lesman.com>
2019-11-11 14:36:06 -0500

    added more APIs to HTML

--- Scripts/site.js
+++ Scripts/site.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.tabs').tabs();
     $('.modal').modal();
+    $('.collapsible').collapsible();
+    drawCharts();
 });
 
 /**
@@ -14,4 +16,58 @@ function switchTab(tabToSwitchTo) {
     var elem = document.querySelector('.tabs');
     var instance = M.Tabs.getInstance(elem);
     instance.select(tabToSwitchTo);
+}
+
+/**
+ * Function to initialize the map for google
+ */
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(39.510510, -84.743011),
+        zoom: 17,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+/**
+ * Charts to draw
+ */
+function drawCharts() {
+    var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
 }
