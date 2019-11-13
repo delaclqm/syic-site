878fc36d460d06ceca474ee783072d03fd4d7cd4
Quaid DeLacluyse <quaidd@lesman.com>
2019-11-13 14:36:11 -0500

    Addded more to news

--- Scripts/site.js
+++ Scripts/site.js
@@ -27,6 +27,29 @@ function myMap() {
         zoom: 17,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    // All properties global to markers
+    var markerProps = {
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: {
+            url: "../assets/GreenSealLogo.png", // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+        }
+    };
+
+    // Our Markers
+    var kofenyaMarker = new google.maps.Marker({
+        ...markerProps,
+        position: new google.maps.LatLng(39.510712, -84.743683),
+        title: 'Kofenya',
+    });
+    var wildBerryMarker = new google.maps.Marker({
+        ...markerProps,
+        position: new google.maps.LatLng(39.510272,-84.7436282),
+        title: 'Wild Berry'
+    });
+    
 }
 
 /**
